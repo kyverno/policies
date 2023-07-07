ef9948d3e2e534a210ae5ddad1fd80480b4334c3
Alok N <alokme123@gmail.com>
2023-07-07 10:21:43 +0000

    fix: bash quotes
    Signed-off-by: Alok N <alokme123@gmail.com>

--- other/label-nodes-cri/label-check.sh
+++ other/label-nodes-cri/label-check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 node=$(kubectl get nodes --no-headers | awk '{print $1}' | head -n 1)
-containerd="$(kubectl get node $node -o json | kyverno jp query "pattern_match('containerd*', status.nodeInfo.containerRuntimeVersion)" | tail -n 1)"
-docker="$(kubectl get node $node -o json | kyverno jp query "pattern_match('docker*', status.nodeInfo.containerRuntimeVersion)" | tail -n 1)"
+containerd=$(kubectl get node "$node" -o json | kyverno jp query "pattern_match('containerd*', status.nodeInfo.containerRuntimeVersion)" | tail -n 1)
+docker=$(kubectl get node "$node" -o json | kyverno jp query "pattern_match('docker*', status.nodeInfo.containerRuntimeVersion)" | tail -n 1)
 if [ "$containerd" = "true" ]; then
-    check="$(kubectl get node $node -o json | kyverno jp query "metadata.labels.runtime=='containerd'" | tail -n 1)";
+    check=$(kubectl get node "$node" -o json | kyverno jp query "metadata.labels.runtime=='containerd'" | tail -n 1);
     if [ "$check" = "true" ]; then 
         echo "Success: node $node labelled runtime: containerd";
         exit 0;
@@ -12,7 +12,7 @@ if [ "$containerd" = "true" ]; then
         exit 1;
     fi;
 elif [ "$docker" = "true" ]; then
-    check="$(kubectl get node $node -o json | kyverno jp query "metadata.labels.runtime=='docker'" | tail -n 1)";
+    check=$(kubectl get node "$node" -o json | kyverno jp query "metadata.labels.runtime=='docker'" | tail -n 1);
     if [ "$check" = "true" ]; then 
         echo "Success: node $node labelled runtime: docker";
         exit 0;
