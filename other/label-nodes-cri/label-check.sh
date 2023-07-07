13ab067a4e50884705d6ef24ed1989d4398e8f9d
Alok N <alokme123@gmail.com>
2023-07-07 08:20:17 +0000

    fix: requested changes
    Signed-off-by: Alok N <alokme123@gmail.com>

--- other/label-nodes-cri/label-check.sh
+++ other/label-nodes-cri/label-check.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-containerd=$(kubectl get node kind-control-plane -o json | kyverno jp query "pattern_match('containerd*', status.nodeInfo.containerRuntimeVersion)" | tail -n 1)
-docker=$(kubectl get node kind-control-plane -o json | kyverno jp query "pattern_match('docker*', status.nodeInfo.containerRuntimeVersion)" | tail -n 1)
+node=$(kubectl get nodes --no-headers | awk '{print $1}' | head -n 1)
+containerd=$(kubectl get node $node -o json | kyverno jp query "pattern_match('containerd*', status.nodeInfo.containerRuntimeVersion)" | tail -n 1)
+docker=$(kubectl get node $node -o json | kyverno jp query "pattern_match('docker*', status.nodeInfo.containerRuntimeVersion)" | tail -n 1)
 if [ "$containerd" = "true" ]; then
-    check=$(kubectl get node kind-control-plane -o json | kyverno jp query "metadata.labels.runtime=='containerd'" | tail -n 1);
+    check=$(kubectl get node $node -o json | kyverno jp query "metadata.labels.runtime=='containerd'" | tail -n 1);
     if [ "$check" = "true" ]; then 
-        echo "Success: node kind-control-plane labelled runtime: containerd";
+        echo "Success: node $node labelled runtime: containerd";
         exit 0;
     else
-        echo "Failed to label node kind-control-plane runtime: containerd";
+        echo "Failed to label node $node runtime: containerd";
         exit 1;
     fi;
 elif [ "$docker" = "true" ]; then
-    check=$(kubectl get node kind-control-plane -o json | kyverno jp query "metadata.labels.runtime=='docker'" | tail -n 1);
+    check=$(kubectl get node $node -o json | kyverno jp query "metadata.labels.runtime=='docker'" | tail -n 1);
     if [ "$check" = "true" ]; then 
-        echo "Success: node kind-control-plane labelled runtime: docker";
+        echo "Success: node $node labelled runtime: docker";
         exit 0;
     else
-        echo "Failed to label node kind-control-plane runtime: docker";
+        echo "Failed to label node $node runtime: docker";
         exit 1;
     fi;
 fi
